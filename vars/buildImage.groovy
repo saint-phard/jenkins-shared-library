85e75ff868e61faf17f5f7c13323c3f66e5bc93b
phard <phard911@gmail.com>
2022-12-20 14:20:02 +0000

    parameterized the image name

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,7 +3,7 @@
 def call(String imageName) {
     echo 'building the docker image'
     withCredentials([usernamePassword(credentialsId: 'my-dockerhub-credentials', usernameVariable: 'username', passwordVariable: 'password')]) {
-        sh "docker build -t $imageName."
+        sh "docker build -t $imageName ."
         sh "echo $password | docker login -u $username --password-stdin"
         sh "docker push $imageName"
     }
