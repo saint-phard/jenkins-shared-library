811573843eddddbac3f9cc0d2df052bc792183f6
phard <phard911@gmail.com>
2022-12-29 12:53:03 +0000

    added dockerLogin and dockerPush classes

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -10,12 +10,20 @@ class Docker implements serializable {
         this.script = script
     }
 
-    def buildImage(string imageName) {
+    def buildImage(String imageName) {
         script.echo 'building the docker image'
-        script.withCredentials([script.usernamePassword(credentialsId: 'my-dockerhub-credentials', usernameVariable: 'username', passwordVariable: 'password')]) {
             script.sh "docker build -t $imageName ."
+        }
+
+    def dockerLogin() {
+        script.echo 'logging in to dockerhub'
+        script.withCredentials([script.usernamePassword(credentialsId: 'my-dockerhub-credentials', usernameVariable: 'username', passwordVariable: 'password')]) {
             script.sh "echo $scriptpassword | docker login -u $script.username --password-stdin"
+            }
+        }
+
+    def dockerPush(String imageName) {
+        script.echo 'pushing docker image to dockerhub'
             script.sh "docker push $imageName"
-    }
-    }
+        }
 }
