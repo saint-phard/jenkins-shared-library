987c93a31e5ab898f5f13cd4a9d77c64d64b9cb7
phard <phard911@gmail.com>
2022-12-30 09:48:16 +0000

    added git push stage info

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -26,4 +26,22 @@ class Docker implements Serializable {
         script.echo 'pushing docker image to dockerhub'
             script.sh "docker push $imageName"
         }
+
+    def githubCommit() {
+        script.echo 'commiting all changes to github to reflect new version changes'
+        script.withCredentials([script.usernamePassword(credentialsId: 'my-github-credentials', usernameVariable: 'username', passwordVariable: 'password')]) {
+            script.sh "echo $script.password | docker login -u $script.username --password-stdin"
+            script.sh 'git config ---global user.name "jenkins-user"'
+            script.sh 'git config ---global user.email "jenkins-user@local.com"'
+
+            script.sh 'git status'
+            script.sh 'git branch'
+            script.sh 'git config --list'
+
+            script.sh "git remote set-url origin https://$script.username:$script.password@https://github.com/saint-phard/maven-docker-jenkins.git"
+            script.sh 'git add .'
+            script.sh 'git commit -m "jenkins-build: app version increment"'
+            script.sh 'git push origin HEAD:jenkins-shared-library'
+            }
+        }
 }
--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,3 +1,7 @@
+#!/usr/bin/env groovy
+
+import com.example.Docker
+
 def call() {
     echo "building the application with maven for $BRANCH_NAME branch"
     sh 'mvn clean package'
